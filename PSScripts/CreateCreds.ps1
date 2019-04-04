0ba2c01cde6721c9f3afaff553c4be8d291a539a
Vimal Shekar <vimalsh@microsoft.com>
2019-04-04 21:24:46 +1100

    changs

--- PSScripts/CreateCreds.ps1
+++ PSScripts/CreateCreds.ps1
@@ -1,1 +1,32 @@
 #hello 
+Function Save-Cred 
+{
+    param(
+        [string] $username, 
+        [string] $pw, 
+        [string] $service
+    )
+
+    if( ( [string]::IsNullOrEmpty($username) -or [string]::IsNullOrEmpty($pw) ) -or [string]::IsNullOrEmpty($service)  )
+    {
+        return $false
+    }
+
+
+    return $true
+}
+
+
+Function Push-Configuration
+{
+    param([string] $configfile)
+
+    if(Test-Path $configfile){
+        $current_config = Get-Content  $configfile | ConvertFrom-Csv
+        foreach($line in $current_config) {
+            write-host $line
+        }
+        return $true
+    }
+    return $false
+}
